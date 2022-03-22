e090c4ed1471ae07789cefa52470cbf7eb3e157e
defstiller <81063003+defstiller@users.noreply.github.com>
2022-03-21 19:25:31 -0500

    deleted not needed files, created first books and css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from "react-dom";
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const books = [{
+  img: "https://images-na.ssl-images-amazon.com/images/I/41E3EKUlXGS._AC_SX184_.jpg",
+  title: "The Judge's List: A Novel (The Whistler Book 2)",
+  author: "John Grisham",
+  description: "lorem ipsum dolor sit amet, consect"
+},
+{
+  img: "https://m.media-amazon.com/images/I/41f5N0Cd5NL._SY346_.jpg",
+  title: "The Midnight Library: A Novel",
+  author: 'Matt Haig'
+}]
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+  return (<>
+  <section className="booklist">
+    {books.map(book => {
+      const {img,title,author,description} = book
+      return (
+        <Book image={img} title={title} author={author}>
+          <Description description={description}/>  
+        </Book>)
+    })}
+  </section>
+  </>)
+}
+
+function Book(props) {
+  const {image, title, author, children} = props
+  return <article className="book"> 
+    <Image source={image} altTitle={title}/>
+    <BookName title={title}/>
+    <Author author={author}/>
+    {children}
+  </article>
+}
+function Image(props) { 
+  const {source, altTitle} = props
+  return <img src={source} alt={`the book called ${altTitle}`} />
+}
+function BookName(props) {
+  return <h2>{props.title}</h2>
+}
+function Author(props) {
+  return  <h3>{props.author}</h3>
+}
+function Description(props) {
+  return <p>{props.description}</p>
+}
+
+ReactDom.render(<BookList/>, document.getElementById("root"))
